d3aa4c620b4f70b0f58f508ba153aa8f55b92f83
NaviHX <navihx@foxmail.com>
2023-10-17 23:17:14 +0800

    add loader module

--- rcore-os/src/loader.rs
+++ rcore-os/src/loader.rs
@@ -1,13 +1,17 @@
-use core::arch::asm;
+use core::arch::{asm, global_asm};
+use lazy_static::lazy_static;
 
 use crate::trap::context::TrapContext;
+use crate::upsync::UPSyncCell;
 
 pub const MAX_APP_NUM: usize = 10;
 pub const APP_BASEADDR: usize = 0x80400000;
 pub const APP_SIZE_LIMIT: usize = 0x20000;
 
+global_asm!(include_str!("link_app.asm"));
+
 /// Load all apps into instruction memory
-pub fn load_apps() {
+pub fn load_apps() -> usize {
     // Find the addresses of each app.
     extern "C" {
         fn _num_app();
@@ -37,6 +41,8 @@ pub fn load_apps() {
             dst.copy_from_slice(src);
         }
     }
+
+    num_app
 }
 
 fn get_app_base_address(id: usize) -> usize {
@@ -46,21 +52,23 @@ fn get_app_base_address(id: usize) -> usize {
 const USER_STACK_SIZE: usize = 0x2000;
 const KERNEL_STACK_SIZE: usize = 0x2000;
 
+#[derive(Copy, Clone)]
 struct KernelStack {
     mem: [u8; KERNEL_STACK_SIZE],
 }
 
+#[derive(Copy, Clone)]
 struct UserStack {
     mem: [u8; USER_STACK_SIZE],
 }
 
-static KERNEL_STACK: KernelStack = KernelStack {
+static KERNEL_STACK: [KernelStack; MAX_APP_NUM] = [KernelStack {
     mem: [0; KERNEL_STACK_SIZE],
-};
+}; MAX_APP_NUM];
 
-static USER_STACK: UserStack = UserStack {
+static USER_STACK: [UserStack; MAX_APP_NUM] = [UserStack {
     mem: [0; USER_STACK_SIZE],
-};
+}; MAX_APP_NUM];
 
 macro_rules! stack_impl {
     ($stack:ty, $size:expr) => {
@@ -81,4 +89,34 @@ impl KernelStack {
         unsafe { *new_sp = ctx };
         unsafe { new_sp.as_mut().unwrap() }
     }
+}
+
+struct AppManager {
+    num_apps: usize,
+}
+
+impl AppManager {
+    pub fn get_num_apps(&self) -> usize {
+        self.num_apps
+    }
+}
+
+lazy_static! {
+    static ref APP_MANAGER: UPSyncCell<AppManager> = unsafe {
+        let num_apps = load_apps();
+
+        UPSyncCell::new(AppManager { num_apps })
+    };
+}
+
+pub fn get_num_apps() -> usize {
+    let app_manager = APP_MANAGER.exclusive_access();
+    app_manager.get_num_apps()
+}
+
+pub fn init_app_context(id: usize) -> usize {
+    KERNEL_STACK[id].push_context(TrapContext::app_init_context(
+        get_app_base_address(id),
+        USER_STACK[id].get_sp(),
+    )) as *const _ as usize
 }
