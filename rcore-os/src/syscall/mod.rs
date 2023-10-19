7d8c45525e1ed8cb04960f047c445569ec5144aa
NaviHX <navihx@foxmail.com>
2023-10-19 14:15:25 +0800

    add kernel get_time syscall

--- rcore-os/src/syscall/mod.rs
+++ rcore-os/src/syscall/mod.rs
@@ -1,9 +1,13 @@
+use self::time::TimeVal;
+
 mod fs;
 mod process;
+mod time;
 
 const SYSCALL_WRITE: usize = 64;
 const SYSCALL_EXIT: usize = 93;
 const SYSCALL_YIELD: usize = 124;
+const SYSCALL_GET_TIME: usize = 169;
 
 pub fn syscall(syscall_id: usize, args: [usize; 3]) -> isize {
     match syscall_id {
@@ -15,6 +19,9 @@ pub fn syscall(syscall_id: usize, args: [usize; 3]) -> isize {
         }
         SYSCALL_YIELD => {
             process::sys_yield()
+        }
+        SYSCALL_GET_TIME => {
+            time::sys_get_time(args[0] as *mut TimeVal, args[1])
         }
         id => {
             panic!("Unsupported syscall id: {id}")
--- rcore-os/src/timer.rs
+++ rcore-os/src/timer.rs
@@ -11,7 +11,8 @@ pub fn set_next_trigger() {
     set_timer(get_time() + CLOCK_FREQ / TICKS_PER_SEC);
 }
 
-const MICRO_PER_SEC: usize = 1_000_000;
+pub const MICRO_PER_SEC: usize = 1_000_000;
+
 pub fn get_time_us() -> usize {
     time::read() / (CLOCK_FREQ / MICRO_PER_SEC)
 }
