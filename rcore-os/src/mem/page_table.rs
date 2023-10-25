fd3278aba62c4db9be95bb86cd782cf32e7ad211
NaviHX <navihx@foxmail.com>
2023-10-25 14:48:20 +0800

    impl page table

---
+++ rcore-os/src/mem/page_table.rs
@@ -0,0 +1,137 @@
+use crate::mem::address::*;
+use alloc::vec::Vec;
+use bitflags::bitflags;
+
+use super::frame_allocator::{frame_alloc, FrameTracker};
+
+bitflags! {
+    struct PTEFlags: u8 {
+        const V = 1 << 0;
+        const R = 1 << 1;
+        const W = 1 << 2;
+        const X = 1 << 3;
+        const U = 1 << 4;
+        const G = 1 << 5;
+        const A = 1 << 6;
+        const D = 1 << 7;
+    }
+}
+
+#[derive(Copy, Clone)]
+#[repr(C)]
+pub struct PageTableEntry {
+    pub bits: usize,
+}
+
+impl PageTableEntry {
+    pub fn new(ppn: PhysPageNum, flags: PTEFlags) -> Self {
+        PageTableEntry {
+            bits: ppn.0 << 10 | flags.bits() as usize,
+        }
+    }
+
+    pub fn empty() -> Self {
+        PageTableEntry { bits: 0 }
+    }
+
+    pub fn ppn(&self) -> PhysPageNum {
+        (self.bits >> 10 & ((1usize << 44) - 1)).into()
+    }
+
+    pub fn flags(&self) -> PTEFlags {
+        PTEFlags::from_bits(self.bits as u8).unwrap()
+    }
+
+    pub fn is_valid(&self) -> bool {
+        (self.flags() & PTEFlags::V).0 != PTEFlags::empty().0
+    }
+}
+
+pub struct PageTable {
+    root_ppn: PhysPageNum,
+    frames: Vec<FrameTracker>,
+}
+
+// TODO impl recursive mapping
+impl PageTable {
+    pub fn new() -> Self {
+        let frame = frame_alloc().expect("Cannot alloc a frame for root page table!");
+        PageTable {
+            root_ppn: frame.ppn,
+            frames: vec![frame],
+        }
+    }
+}
+
+// TODO impl recursive mapping
+impl PageTable {
+    fn find_pte_create(&mut self, vpn: VirtPageNum) -> Option<&mut PageTableEntry> {
+        let idxs = vpn.indexes();
+        let ppn = self.root_ppn;
+        let mut result = None;
+
+        #[allow(clippy::needless_range_loop)]
+        for i in 0..3 {
+            let idx = idxs[i];
+            let pte = &mut ppn.get_pte_table()[idx];
+            if i == 2 {
+                result = Some(pte);
+                break;
+            }
+            if !pte.is_valid() {
+                let frame = frame_alloc().expect("No un-allocated frame for page table entry");
+                *pte = PageTableEntry::new(frame.ppn, PTEFlags::V);
+                self.frames.push(frame);
+            }
+        }
+
+        result
+    }
+
+    fn find_pte(&self, vpn: VirtPageNum) -> Option<&mut PageTableEntry> {
+        let idxs = vpn.indexes();
+        let ppn = self.root_ppn;
+        let mut result = None;
+
+        #[allow(clippy::needless_range_loop)]
+        for i in 0..3 {
+            let idx = idxs[i];
+            let pte = &mut ppn.get_pte_table()[idx];
+            if i == 2 {
+                result = Some(pte);
+                break;
+            }
+            if !pte.is_valid() {
+                return None;
+            }
+        }
+
+        result
+    }
+}
+
+impl PageTable {
+    pub fn map(&mut self, vpn: VirtPageNum, ppn: PhysPageNum, flags: PTEFlags) {
+        let pte = self.find_pte_create(vpn).unwrap();
+        assert!(!pte.is_valid(), "vpn {:?} is mapped before mapping!", vpn);
+        *pte = PageTableEntry::new(ppn, flags | PTEFlags::V);
+    }
+    pub fn unmap(&mut self, vpn: VirtPageNum) {
+        let pte = self.find_pte(vpn).unwrap();
+        assert!(pte.is_valid(), "vpn {:?} is invalid before unmapping!", vpn);
+        *pte = PageTableEntry::empty();
+    }
+}
+
+impl PageTable {
+    pub fn from_token(satp: usize) -> Self {
+        Self {
+            root_ppn: PhysPageNum::from(satp & ((1usize << 44) - 1)),
+            frames: Vec::new(),
+        }
+    }
+
+    pub fn translate(&self, vpn: VirtPageNum) -> Option<PageTableEntry> {
+        self.find_pte(vpn).map(|pte| *pte)
+    }
+}
