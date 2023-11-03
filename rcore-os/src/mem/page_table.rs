79e89dcfe1b4acbe57e6f0058fb245e42421b23f
NaviHX <navihx@foxmail.com>
2023-11-03 13:39:54 +0800

    get page table from addr and translate user buffer to phys addr

--- rcore-os/src/mem/page_table.rs
+++ rcore-os/src/mem/page_table.rs
@@ -5,7 +5,7 @@ use bitflags::bitflags;
 use super::frame_allocator::{frame_alloc, FrameTracker};
 
 bitflags! {
-    struct PTEFlags: u8 {
+    pub struct PTEFlags: u8 {
         const V = 1 << 0;
         const R = 1 << 1;
         const W = 1 << 2;
@@ -134,4 +134,38 @@ impl PageTable {
     pub fn translate(&self, vpn: VirtPageNum) -> Option<PageTableEntry> {
         self.find_pte(vpn).map(|pte| *pte)
     }
+}
+
+impl PageTable {
+    pub fn token(&self) -> usize {
+        8usize << 60 | self.root_ppn.0
+    }
+}
+
+pub fn translate_byte_buffer(
+    token: usize,
+    ptr: *const u8,
+    len: usize,
+) -> Vec<&'static mut [u8]> {
+    let page_table = PageTable::from_token(token);
+    let mut start = ptr as usize;
+    let end = start + len;
+    let mut v = vec![];
+
+    while start < end {
+        let start_va = VirtAddr::from(start);
+        let mut vpn = start_va.floor();
+        let ppn = page_table.translate(vpn).unwrap().ppn();
+        vpn.step();
+        let mut end_va = VirtAddr::from(end).min(vpn.into());
+        if end_va.page_offset() == 0 {
+            v.push(&mut ppn.get_byte_array()[start_va.page_offset()..]);
+        } else {
+            v.push(&mut ppn.get_byte_array()[start_va.page_offset()..end_va.page_offset()])
+        }
+
+        start = end_va.into();
+    }
+
+    v
 }
