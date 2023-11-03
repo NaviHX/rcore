0b219aeb1040007c68ff161c38495cf18efe643d
NaviHX <navihx@foxmail.com>
2023-11-03 13:50:54 +0800

    extend task control block

--- rcore-os/src/task/context.rs
+++ rcore-os/src/task/context.rs
@@ -1,3 +1,5 @@
+use crate::trap::trap_return;
+
 #[repr(C)]
 #[derive(Copy, Clone, Default)]
 pub struct TaskContext {
@@ -9,12 +11,32 @@ pub struct TaskContext {
 // Receive an sp pointing to TrapContext in Kernenl Stack
 // Wrap the init trap restore context to task context
 pub fn goto_restore(kernel_sp: usize) -> TaskContext {
-    extern "C" { fn __restore(); }
+    extern "C" {
+        fn __restore();
+    }
     let __restore = __restore as usize;
 
     TaskContext {
         ra: __restore,
         sp: kernel_sp,
         s: [0; 12],
+    }
+}
+
+impl TaskContext {
+    pub fn zero_init() -> Self {
+        Self {
+            ra: 0,
+            sp: 0,
+            s: [0; 12],
+        }
+    }
+
+    pub fn goto_trap_return(kernel_stack_ptr: usize) -> Self {
+        Self {
+            ra: trap_return as usize,
+            sp: kernel_stack_ptr,
+            s: [0; 12],
+        }
     }
 }
--- rcore-os/src/task/mod.rs
+++ rcore-os/src/task/mod.rs
@@ -2,11 +2,19 @@ pub mod context;
 pub mod switch;
 
 use crate::{
-    loader::{self, MAX_APP_NUM},
+    config::{PAGE_SIZE, TRAMPOLINE, TRAP_CONTEXT},
+    loader::{self, get_app_data, KERNEL_STACK_SIZE, MAX_APP_NUM},
     log,
+    mem::{
+        address::{PhysPageNum, VirtAddr},
+        memory_set::{MapPermission, MemorySet},
+        KERNEL_SPACE,
+    },
     sbi::shutdown,
+    trap::{context::TrapContext, trap_handler},
     upsync::UPSyncCell,
 };
+use alloc::vec::Vec;
 use context::TaskContext;
 use lazy_static::lazy_static;
 
@@ -20,10 +28,13 @@ pub enum TaskStatus {
     Exited,
 }
 
-#[derive(Copy, Clone)]
 pub struct TaskControlBlock {
     pub task_status: TaskStatus,
     pub task_context: TaskContext,
+
+    pub memory_set: MemorySet,
+    pub trap_context_ppn: PhysPageNum,
+    pub base_size: usize,
 }
 
 pub struct TaskManager {
@@ -32,14 +43,14 @@ pub struct TaskManager {
 }
 
 struct InnerTaskManager {
-    tasks: [TaskControlBlock; MAX_APP_NUM],
-    current_app: usize,
+    tasks: Vec<TaskControlBlock>,
+    current_task: usize,
 }
 
 impl TaskManager {
     pub fn run_first_task(&self) {
         let mut inner = self.inner.exclusive_access();
-        inner.current_app = 0;
+        inner.current_task = 0;
         let task0 = &mut inner.tasks[0];
         task0.task_status = TaskStatus::Running;
         let next_task_context_ptr = &task0.task_context as *const _;
@@ -54,13 +65,13 @@ impl TaskManager {
 
     fn mark_current_suspended(&self) {
         let mut inner = self.inner.exclusive_access();
-        let current_task_id = inner.current_app;
+        let current_task_id = inner.current_task;
         inner.tasks[current_task_id].task_status = TaskStatus::Ready;
     }
 
     fn mark_current_exited(&self) {
         let mut inner = self.inner.exclusive_access();
-        let current_task_id = inner.current_app;
+        let current_task_id = inner.current_task;
         inner.tasks[current_task_id].task_status = TaskStatus::Exited;
     }
 
@@ -68,7 +79,7 @@ impl TaskManager {
     /// Now we just schedule the first ready task after current task
     fn find_next_task(&self) -> Option<usize> {
         let inner = self.inner.exclusive_access();
-        let current = inner.current_app;
+        let current = inner.current_task;
         let len = self.num_app;
         (current + 1..=current + len) // include the current app itself
             .map(|id| id % len)
@@ -78,8 +89,8 @@ impl TaskManager {
     pub fn run_next_task(&self) {
         if let Some(next_task_id) = self.find_next_task() {
             let mut inner = self.inner.exclusive_access();
-            let cur_id = inner.current_app;
-            inner.current_app = next_task_id;
+            let cur_id = inner.current_task;
+            inner.current_task = next_task_id;
             let next_task = &mut inner.tasks[next_task_id];
             next_task.task_status = TaskStatus::Running;
             let next_task_context_ptr = &next_task.task_context as *const _;
@@ -96,23 +107,19 @@ impl TaskManager {
 
 lazy_static! {
     static ref TASK_MANAGER: TaskManager = {
-        let num_apps = loader::get_num_apps();
-        let mut tasks = [TaskControlBlock {
-            task_status: TaskStatus::UnInit,
-            task_context: TaskContext::default(),
-        }; MAX_APP_NUM];
-        for i in 0..num_apps {
+        let num_app = loader::get_num_app();
+        let mut tasks: Vec<TaskControlBlock> = vec![];
+        for i in 0..num_app {
             // Init tasks and set Ready
-            tasks[i].task_context = context::goto_restore(loader::init_app_context(i));
-            tasks[i].task_status = TaskStatus::Ready;
+            tasks.push(TaskControlBlock::new(get_app_data(i), i));
         }
 
         TaskManager {
-            num_app: num_apps,
+            num_app,
             inner: unsafe {
                 UPSyncCell::new(InnerTaskManager {
                     tasks,
-                    current_app: 0,
+                    current_task: 0,
                 })
             }
         }
@@ -132,4 +139,75 @@ pub fn exit_and_run_next() -> ! {
     TASK_MANAGER.mark_current_exited();
     TASK_MANAGER.run_next_task();
     panic!("Run exited task again");
+}
+
+/// Return Both the bottom and top of the kernel stack of app_id
+pub fn kernel_stack_position(app_id: usize) -> (usize, usize) {
+    let top = TRAMPOLINE - app_id * (KERNEL_STACK_SIZE + PAGE_SIZE);
+    let btm = top - KERNEL_STACK_SIZE;
+    (btm, top)
+}
+
+impl TaskControlBlock {
+    pub fn get_trap_context(&self) -> &'static mut TrapContext {
+        self.trap_context_ppn.get_mut()
+    }
+    pub fn new(elf_data: &[u8], app_id: usize) -> Self {
+        let (memory_set, user_sp, entry_point) = MemorySet::from_elf(elf_data);
+        let trap_context_ppn = memory_set
+            .translate(VirtAddr::from(TRAP_CONTEXT).into())
+            .unwrap()
+            .ppn();
+        let task_status = TaskStatus::Ready;
+        let (kernel_stack_bottom, kernel_stack_top) = kernel_stack_position(app_id);
+        KERNEL_SPACE.exclusive_access().insert_framed_area(
+            VirtAddr(kernel_stack_bottom),
+            VirtAddr(kernel_stack_top),
+            MapPermission::R | MapPermission::W,
+        );
+
+        let task_control_block = Self {
+            task_status,
+            task_context: TaskContext::goto_trap_return(kernel_stack_top),
+            memory_set,
+            trap_context_ppn,
+            base_size: user_sp,
+        };
+
+        let trap_context = task_control_block.get_trap_context();
+        *trap_context = TrapContext::app_init_context(
+            entry_point,
+            user_sp,
+            KERNEL_SPACE.exclusive_access().token(),
+            kernel_stack_top,
+            trap_handler as usize,
+        );
+
+        task_control_block
+    }
+
+    pub fn get_user_token(&self) -> usize {
+        self.memory_set.token()
+    }
+}
+
+impl TaskManager {
+    fn get_current_token(&self) -> usize {
+        let inner = self.inner.borrow();
+        let current = inner.current_task;
+        inner.tasks[current].get_user_token()
+    }
+
+    fn get_current_trap_context(&self) -> &mut TrapContext {
+        let inner = self.inner.borrow();
+        let current = inner.current_task;
+        inner.tasks[current].get_trap_context()
+    }
+}
+
+pub fn get_current_token() -> usize {
+    TASK_MANAGER.get_current_token()
+}
+pub fn get_current_trap_context() -> &'static mut TrapContext {
+    TASK_MANAGER.get_current_trap_context()
 }
