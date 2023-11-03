cd0f472274a3645b7f19e495e1e9f5f52eab9a8f
NaviHX <navihx@foxmail.com>
2023-11-03 13:40:55 +0800

    make kernel memory set and mem init func

--- rcore-os/src/mem/mod.rs
+++ rcore-os/src/mem/mod.rs
@@ -2,3 +2,12 @@ pub mod heap_allocator;
 pub mod frame_allocator;
 pub mod address;
 pub mod page_table;
+pub mod memory_set;
+
+pub use memory_set::KERNEL_SPACE;
+
+pub fn init() {
+    heap_allocator::init_heap();
+    frame_allocator::init_frame_allocator();
+    KERNEL_SPACE.exclusive_access().activate();
+}
