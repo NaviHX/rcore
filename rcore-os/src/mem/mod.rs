8735a5d0abde0ee9550110477715fc6ea61975b1
NaviHX <navihx@foxmail.com>
2023-11-08 13:27:13 +0800

    add log output

--- rcore-os/src/mem/mod.rs
+++ rcore-os/src/mem/mod.rs
@@ -6,8 +6,13 @@ pub mod memory_set;
 
 pub use memory_set::KERNEL_SPACE;
 
+use crate::log;
+
 pub fn init() {
     heap_allocator::init_heap();
+    log!("Heap allocator inited");
     frame_allocator::init_frame_allocator();
+    log!("Frame allocator inited");
     KERNEL_SPACE.exclusive_access().activate();
+    log!("Kernel memory set inited");
 }
