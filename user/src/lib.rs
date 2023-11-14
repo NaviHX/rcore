b5fdc132f348d77bafffbc260f3dfb5e7d9dbe50
NaviHX <navihx@foxmail.com>
2023-11-14 14:10:54 +0800

    heap allocation support

--- user/src/lib.rs
+++ user/src/lib.rs
@@ -1,17 +1,34 @@
 #![no_std]
 #![feature(linkage)]
 #![feature(panic_info_message)]
+#![feature(alloc_error_handler)]
 
+pub mod console;
+mod lang_items;
+pub mod process;
 mod syscall;
 pub mod time;
-pub mod console;
-pub mod process;
-mod lang_items;
+
+use buddy_system_allocator::LockedHeap;
+const USER_HEAP_SIZE: usize = 16384;
+static mut HEAP_SPACE: [u8; USER_HEAP_SIZE] = [0; USER_HEAP_SIZE];
+
+#[global_allocator]
+static HEAP: LockedHeap<32> = LockedHeap::empty();
+
+#[alloc_error_handler]
+pub fn handle_alloc_error(layout: core::alloc::Layout) -> ! {
+    panic!("Heap allocation error, layout = {:?}", layout);
+}
 
 #[no_mangle]
 #[link_section = ".text.entry"]
 pub extern "C" fn _start() -> ! {
     clear_bss();
+    unsafe {
+        HEAP.lock()
+            .init(HEAP_SPACE.as_ptr() as usize, USER_HEAP_SIZE);
+    }
     exit(main());
     panic!("The application should have exited!");
 }
@@ -22,9 +39,7 @@ fn clear_bss() {
         fn bss_end();
     }
 
-    (bss_start as usize..bss_end as usize).for_each(|p| {
-        unsafe { (p as *mut u8).write_volatile(0) }
-    })
+    (bss_start as usize..bss_end as usize).for_each(|p| unsafe { (p as *mut u8).write_volatile(0) })
 }
 
 #[no_mangle]
@@ -35,8 +50,14 @@ fn main() -> i32 {
 
 use syscall::*;
 
-pub fn write(fd: usize, buf: &[u8]) -> isize { sys_write(fd, buf) }
-pub fn read(fd: usize, buf: &mut[u8]) -> isize { sys_read(fd, buf) }
-pub fn exit(xstate: i32) -> isize { sys_exit(xstate) }
+pub fn write(fd: usize, buf: &[u8]) -> isize {
+    sys_write(fd, buf)
+}
+pub fn read(fd: usize, buf: &mut [u8]) -> isize {
+    sys_read(fd, buf)
+}
+pub fn exit(xstate: i32) -> isize {
+    sys_exit(xstate)
+}
 
 pub use process::*;
