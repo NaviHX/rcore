2289aff74d609976a526dd3946c1eb3681596170
NaviHX <navihx@foxmail.com>
2023-11-19 14:44:43 +0800

    add super block layout

---
+++ easy-fs/src/layout.rs
@@ -0,0 +1,9 @@
+#[repr(C)]
+pub struct SuperBlock {
+    magic: u32,
+    pub total_blocks: u32,
+    pub inode_bitmap_blocks: u32,
+    pub inode_area_blocks: u32,
+    pub data_bitmap_blocks: u32,
+    pub data_area_blocks: u32,
+}
--- easy-fs/src/lib.rs
+++ easy-fs/src/lib.rs
@@ -3,5 +3,6 @@ extern crate alloc;
 
 pub mod block_dev;
 pub mod block_cache;
+pub mod layout;
 
 pub const BLOCK_SIZE: usize = 512;
