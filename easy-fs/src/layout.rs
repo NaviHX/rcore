f17d9f6aef112cba3f1e8922c90a095e911c2ea5
NaviHX <navihx@foxmail.com>
2023-11-19 14:51:03 +0800

    add super block function

--- easy-fs/src/layout.rs
+++ easy-fs/src/layout.rs
@@ -7,3 +7,29 @@ pub struct SuperBlock {
     pub data_bitmap_blocks: u32,
     pub data_area_blocks: u32,
 }
+
+pub const EFS_MAGIC: u32 = 0xdead_beef;
+
+impl SuperBlock {
+    pub fn initialize(
+        &mut self,
+        total_blocks: u32,
+        inode_bitmap_blocks: u32,
+        inode_area_blocks: u32,
+        data_bitmap_blocks: u32,
+        data_area_blocks: u32,
+    ) {
+        *self = Self {
+            magic: EFS_MAGIC,
+            total_blocks,
+            inode_bitmap_blocks,
+            inode_area_blocks,
+            data_bitmap_blocks,
+            data_area_blocks,
+        }
+    }
+
+    pub fn is_valid(&self) -> bool {
+        self.magic == EFS_MAGIC
+    }
+}
